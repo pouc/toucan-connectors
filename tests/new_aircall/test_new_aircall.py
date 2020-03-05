933beeb0a920e81ff9a495b2291f3df63ac95a44
Rachelle Wood <archaurore@gmail.com>
2020-03-05 18:14:33 +0100

    tested basic function being called from what will become the request builder

--- tests/new_aircall/test_new_aircall.py
+++ tests/new_aircall/test_new_aircall.py
@@ -1,8 +1,16 @@
 import pytest
 
-from toucan_connectors.new_aircall.new_aircall_connector import New_aircallConnector, New_aircallDataSource
+from toucan_connectors.new_aircall.new_aircall_connector import New_aircallConnector, New_aircallDataSource, build_aircall_request_url, BASE_AIRCALL_API_URL
 
 
 def test_get_df():
     pass
 
+def test_set_up_request(mocker):
+    """
+    This tests basic request builder
+    """
+    fake_conn = New_aircallConnector(name="Baz")
+    mah_string = fake_conn.set_up_request("foo")
+
+    assert mah_string == "https://api.aircall.io/v1/foo"
--- toucan_connectors/new_aircall/new_aircall_connector.py
+++ toucan_connectors/new_aircall/new_aircall_connector.py
@@ -1,7 +1,19 @@
+# AirCall Business-in-a-Box connector
+# In development
+
+# ***** IMPORTS *****
+
+# VENDOR
+import asyncio
 import pandas as pd
 
+# CONNECTOR MODULES
 from toucan_connectors.toucan_connector import ToucanConnector, ToucanDataSource
 
+BASE_AIRCALL_API_URL = "https://api.aircall.io/v1"
+
+def build_aircall_request_url(route):
+    return f'{BASE_AIRCALL_API_URL}/{route}'
 
 class New_aircallDataSource(ToucanDataSource):
     query: str
@@ -10,8 +22,12 @@ class New_aircallDataSource(ToucanDataSource):
 class New_aircallConnector(ToucanConnector):
     data_source_model: New_aircallDataSource
 
-    username: str
-    password: str
+    # username: str
+    # password: str
 
     def _retrieve_data(self, data_source: New_aircallDataSource) -> pd.DataFrame:
         pass
+
+    def set_up_request(self, route):
+        aircall_request_url = build_aircall_request_url(route)
+        return aircall_request_url
