ab3268d3be484b99aac472511d9826aa071f38bb
Raphaël <raphael.vignes@toucantoco.com>
2020-11-30 18:11:27 +0100

    fix(CI): added upgrade pip step in CI workflow

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ extras_require = {
     'github': ['python_graphql_client'],
     'google_analytics': ['google-api-python-client', 'oauth2client'],
     'google_big_query': ['pandas_gbq'],
-    'google_cloud_mysql': ['PyMySQL>=0.8.0'],
+    'google_cloud_mysql': ['PyMySQL==0.10.0'],
     'google_my_business': ['google-api-python-client>=1.7.5'],
     'google_sheets': bearer_deps,
     'google_spreadsheet': ['gspread>=3', 'oauth2client'],
